5121b8039042e3413005ac45b68b83d2420c6b35
SD-Jonatha <thiago.infortec@gmail.com>
2023-01-19 01:20:31 -0300

    Caso de uso do cadastro de imagens do carro

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,7 +1,9 @@
+import {Router} from "express";
+import multer from "multer";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarsSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listaAvailableCars/ListAvailableCarsController";
-import {Router} from "express";
+import { UploadCarImageController } from "@modules/cars/useCases/UploadCarImage/UploadCarImageController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -10,10 +12,14 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
+
+const upload = multer({ dest: "./tmp/cars" });
 
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle );
 carsRoutes.get("/available", listAvailableCarsController.handle);
 
 carsRoutes.post("/specifications/:id", ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle);
+carsRoutes.post("/images/:id", ensureAuthenticated, ensureAdmin, upload.array("images"), uploadCarImageController.handle)
 
 export {carsRoutes}
