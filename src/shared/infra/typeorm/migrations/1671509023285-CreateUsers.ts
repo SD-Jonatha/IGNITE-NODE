a8b39e61fa16f693856e49e5a7746e1d4cd32f96
SD-Jonatha <thiago.infortec@gmail.com>
2023-02-03 15:45:02 -0300

    Criando migrations do aluguel

--- src/shared/infra/typeorm/migrations/1671509023285-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1671509023285-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1671509023285 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
